45c037369f8c9aead54ec8c5fdf66ccb0d328ab0
Andrew Bayer <andrew.bayer@gmail.com>
2016-12-14 08:08:21 -0800

    Add agent none reference

--- 02-agentAny.groovy
+++ 02-agentAny.groovy
@@ -1,4 +1,6 @@
 pipeline {
+    // If we'd gone with "agent none", the sh step below would fail,
+    // since we wouldn't be running on an agent!
     agent any
     stages {
         stage("foo") {
